6f41ac45410034c86cec532630ebe44bb356fbee
AIMINTU <minto.aminul4@hotmail.com>
2024-10-22 09:21:32 +0200

    class-24

---
+++ index.js
@@ -0,0 +1,48 @@
+// console.log("this is from developer branch")
+// console.log("this is from developer branch")
+// function nayeem(){
+//     console.log("paise")
+//     document.body.style.backgroundColor="red";
+
+// }
+//addEventListener
+// const byttan=document.getElementById('btn').addEventListener('click',document.body.style.backgroundColor='purple')
+// console.log(byttan)
+
+
+// document.getElementById('btn').addEventListener('click',function nimul(){
+//     console.log("manus");
+// })
+
+document.getElementById('btn').addEventListener('click',function niyamul(){
+    document.body.style.backgroundColor='violet'
+})
+
+// function nayeem(){
+//     //console.log('paiche')
+//     document.body.style.backgroundColor='violet';
+// }
+// const nayeem=document.getElementById('btn').addEventListener('click',document.body.style.backgroundColor='red');
+// const nayeem=document.getElementById('btn').addEventListener('click',document.body.style.backgroundColor='purple');
+// console.log(nayeem)
+// document.getElementById('btn').addEventListener('click',function nayeem()){
+//     document.body.style.backgroundColor='purple'
+// }
+// document.getElementById('btn').addEventListener('click')
+
+// const title=document.getElementById('btn')
+// title.addEventListener('click',function nayeem(){
+//     console.log('nayeem')
+// })
+// const title1=document.getElementById('btn')
+// title1.addEventListener('click',function(){
+    
+
+// })
+const title=document.getElementById('btn2')
+title.addEventListener('click',function(){
+    const para=document.getElementById('para') //vai ami eikhane buji nai apni get korlen button ke kintu text change korlen papa theke.
+    console.log(para)
+    para.innerText="ami javascript theke belong kori"
+
+})
